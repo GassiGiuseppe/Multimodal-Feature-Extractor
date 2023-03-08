06fb40450e5638722f06bb326fad6c039a7f3d8a
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2023-03-08 15:20:48 +0100

    the attributes set to protected and corrected the torch models layer usage

--- src/visual/models/CnnFeatureExtractor.py
+++ src/visual/models/CnnFeatureExtractor.py
@@ -13,54 +13,57 @@ from src.utils.model_map import tensorflow_models_for_extraction, torch_models_f
 # the file is heavily cut, the complete file is in the 'old' directory
 class CnnFeatureExtractor:
     def __init__(self, gpu='-1'):
-        self.framework_list = None
+        self._framework_list = None
         self.model = None
-        self.output_layer = None
-        self.model_name = None
-        self.gpu = gpu
+        self._output_layer = None
+        self._model_name = None
+        self._gpu = gpu
 
-        self.device = torch.device("cuda:" + str(self.gpu) if self.gpu != '-1' else "cpu")
+        self._device = torch.device("cuda:" + str(self._gpu) if self._gpu != '-1' else "cpu")
 
         # self.imagenet = imagenet
         # self.pca = PCA(n_components=pca_dim)
         # self.reshape = reshape
 
     def set_model(self, model_name):
-        self.model_name = model_name
-        if self.model_name in tensorflow_models_for_extraction and 'tensorflow' in self.framework_list:
-            self.model = tensorflow_models_for_extraction[self.model_name]()
-        elif self.model_name in torch_models_for_extraction and 'torch' in self.framework_list:
-            self.model = torch_models_for_extraction[self.model_name](pretrained=True)
-            self.model.to(self.device)
+        self._model_name = model_name
+        if self._model_name in tensorflow_models_for_extraction and 'tensorflow' in self._framework_list:
+            self.model = tensorflow_models_for_extraction[self._model_name]()
+        elif self._model_name in torch_models_for_extraction and 'torch' in self._framework_list:
+            self.model = torch_models_for_extraction[self._model_name](pretrained=True)
+            self.model.to(self._device)
             self.model.eval()
         else:
             raise NotImplemented('This feature extractor has not been added yet!')
 
     def set_output_layer(self, output_layer):
-        self.output_layer = output_layer
+        self._output_layer = output_layer
 
     def set_framework(self, framework_list):
-        self.framework_list = framework_list
+        self._framework_list = framework_list
 
     def extract_feature(self, image):
-        if self.model_name in tensorflow_models_for_extraction and 'tensorflow' in self.framework_list:
+        if self._model_name in tensorflow_models_for_extraction and 'tensorflow' in self._framework_list:
             input_model = self.model.input
-            output_layer = self.model.get_layer(self.output_layer).output
+            output_layer = self.model.get_layer(self._output_layer).output
             output = tf.keras.Model(input_model, output_layer)(image, training=False)
             # update the framework list
-            self.framework_list = ['tensorflow']
+            self._framework_list = ['tensorflow']
 
         else:
             # torch models??? the layer is linked nowhere
             s1 = torch.nn.Sequential(*list(self.model.children())[:-1])
             s2 = torch.nn.Flatten()
-            s3 = torch.nn.Sequential(*list(list(self.model.children())[-1][1:-self.output_layer]))
+            if isinstance(list(self.model.children())[-1], torch.nn.Linear):
+                s3 = list(self.model.children())[-self._output_layer]
+            else:
+                s3 = torch.nn.Sequential(*list(list(self.model.children())[-1][1:-self._output_layer]))
             feature_model = torch.nn.Sequential(s1, s2, s3)
             feature_model.eval()
             output = np.squeeze(feature_model(
-                image[None, ...].to(self.device)
+                image[None, ...].to(self._device)
             ).data.cpu().numpy())
             # update the framework list
-            self.framework_list = ['torch']
+            self._framework_list = ['torch']
 
         return output
