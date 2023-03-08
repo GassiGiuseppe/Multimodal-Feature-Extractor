2149b0695e1529558030f75d20aac1578cb774d3
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2023-03-08 15:23:00 +0100

    the attributes set to protected

--- src/visual/VisualDataset.py
+++ src/visual/VisualDataset.py
@@ -13,7 +13,7 @@ class VisualDataset(DatasetFather, ABC):
 
     def __init__(self, input_directory_path, output_directory_path, model_name='VGG19', reshape=(224, 224)):
         super().__init__(input_directory_path, output_directory_path, model_name)
-        self.__reshape = reshape
+        self._reshape = reshape
 
     def __getitem__(self, idx):
         image_path = os.path.join(self._input_directory_path, self._filenames[idx])
@@ -24,7 +24,7 @@ class VisualDataset(DatasetFather, ABC):
 
         norm_sample = self._pre_processing(sample)
 
-        if 'tensorflow' in self.framework_list:
+        if 'tensorflow' in self._framework_list:
             # np for tensorflow
             return np.expand_dims(norm_sample, axis=0)
         else:
@@ -33,16 +33,16 @@ class VisualDataset(DatasetFather, ABC):
 
     def _pre_processing(self, sample):
         # resize
-        if self.__reshape:
-            res_sample = sample.resize(self.__reshape, resample=Image.BICUBIC)
+        if self._reshape:
+            res_sample = sample.resize(self._reshape, resample=Image.BICUBIC)
         else:
             res_sample = sample
 
-        if self._model_name in tensorflow_models_for_normalization and 'tensorflow' in self.framework_list:
+        if self._model_name in tensorflow_models_for_normalization and 'tensorflow' in self._framework_list:
             command = tensorflow_models_for_normalization[self._model_name]
             norm_sample = command.preprocess_input(np.array(res_sample))
             # update the framework list
-            self.framework_list = ['tensorflow']
+            self._framework_list = ['tensorflow']
         else:
             # if the model is a torch model, the normalization is the same for everyone
             transform = transforms.Compose([transforms.ToTensor(),
@@ -51,12 +51,12 @@ class VisualDataset(DatasetFather, ABC):
                                             ])
             norm_sample = transform(res_sample)
             # update the framework list
-            self.framework_list = ['torch']
+            self._framework_list = ['torch']
 
         return norm_sample
 
     def set_reshape(self, reshape):
-        self.__reshape = reshape
+        self._reshape = reshape
 
     def get_image_name(self, index):
         return self._filenames[index]
