b07c70e159068e79f60b9c6d154c9df916b03f90
GassiGiuseppe <g.gassi@studenti.poliba.it>
2023-05-24 19:06:34 +0200

    file comments update

--- src/multimodal/audio/AudioDataset.py
+++ src/multimodal/audio/AudioDataset.py
@@ -11,10 +11,21 @@ from transformers import Wav2Vec2Processor
 class AudioDataset(DatasetFather):
 
     def __init__(self, input_directory_path, output_directory_path):
+        """
+        Manage the Audio Dataset (folder of input and folder of output).
+        It will Manage data of input (and their preprocessing), and data of output
+        :param input_directory_path: folder of the input data to elaborate as String
+        :param output_directory_path: folder of where put Output as String, it will be created if does not exist
+        """
         super().__init__(input_directory_path, output_directory_path, model_name=None)
         self._model_for_preprocessing = None
 
     def __getitem__(self, index):
+        """
+        It retrieves a sample preprocessed given its id (the id refers to the sorted filenames)
+        :param index: Integer, indicates the number associated to the file o elaborate
+        :return: the audio resembled (preprocessed) and its sample rate. [audio, rate]
+        """
         audio_path = os.path.join(self._input_directory_path, self._filenames[index])
 
         # right now both torchaudio and transformers do the same thing, but it is preferable to keep them work
@@ -28,9 +39,21 @@ class AudioDataset(DatasetFather):
             return self._pre_processing([audio, sample_rate])
 
     def set_model(self, model):
+        """
+        sets the model as a string to execute the preprocessing
+        NOTE ON MODELS:
+        here it is accepted torchaudio and transformers (by huggingface) models. When using transformers you have to
+        indicate in the String also the repo as 'repo/model_name'
+        :param model: the model name as a String
+        """
         self._model_name = model
 
     def _pre_processing(self, pre_process_input):
+        """
+        It resample the audio to a rate that is the same to the one with the models where trained
+        :param pre_process_input: blob of data (audio wave and audio rate )
+        :return: [preprocessed audio, new sample rate]
+        """
         audio = pre_process_input[0]
         rate = pre_process_input[1]
         if 'torch' in self._framework_list or 'torchaudio' in self._framework_list:
